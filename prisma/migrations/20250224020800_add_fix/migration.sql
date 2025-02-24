03aa322c2bfdc55a4fab114b3fbab40e302e918d
carlos-rodrigues-tl <carlos-rock012@hotmail.com>
2025-02-24 19:59:36 -0300

    Primeiro commit

---
+++ prisma/migrations/20250224020800_add_fix/migration.sql
@@ -0,0 +1,48 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createAt` on the `MenuCategory` table. All the data in the column will be lost.
+  - You are about to drop the column `updateAt` on the `MenuCategory` table. All the data in the column will be lost.
+  - You are about to drop the column `createAt` on the `Order` table. All the data in the column will be lost.
+  - You are about to drop the column `updateAt` on the `Order` table. All the data in the column will be lost.
+  - You are about to drop the column `createAt` on the `OrderProduct` table. All the data in the column will be lost.
+  - You are about to drop the column `updateAt` on the `OrderProduct` table. All the data in the column will be lost.
+  - You are about to drop the column `createAt` on the `Product` table. All the data in the column will be lost.
+  - You are about to drop the column `updateAt` on the `Product` table. All the data in the column will be lost.
+  - You are about to drop the column `createAt` on the `Restaurant` table. All the data in the column will be lost.
+  - You are about to drop the column `updateAt` on the `Restaurant` table. All the data in the column will be lost.
+  - Added the required column `customerCpf` to the `Order` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `customerName` to the `Order` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "MenuCategory" DROP COLUMN "createAt",
+DROP COLUMN "updateAt",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "Order" DROP COLUMN "createAt",
+DROP COLUMN "updateAt",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "customerCpf" TEXT NOT NULL,
+ADD COLUMN     "customerName" TEXT NOT NULL,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "OrderProduct" DROP COLUMN "createAt",
+DROP COLUMN "updateAt",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "Product" DROP COLUMN "createAt",
+DROP COLUMN "updateAt",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "Restaurant" DROP COLUMN "createAt",
+DROP COLUMN "updateAt",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
